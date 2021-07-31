4afa51b0848aa48b0e674e1b11ab3a7888f75c51
rizkikhair <rizki.khair@tradaru.com>
2021-07-31 21:38:57 +0700

    fix portofolio

--- src/components/portofolio/main.vue
+++ src/components/portofolio/main.vue
@@ -22,54 +22,27 @@
 
     <transition name="scale" tag="div" >
       <div v-if="!loading" class="row">
-          <div
-            v-for="(portofolio, n) in portofolioFiltered"
-            :key="'port' + n"
-            :class="{ 'is-hidden': !portofolio.active }"
-            class="col portofolio my-8">
-            <img
-              v-if="portofolio.class === 'sk-akuntansi'"
-              src="@/assets/img/sertifikat akuntansi_001.jpg"
-              class="img-fluid"
-            />
-            <img
-              v-else-if="portofolio.class === 'sk-excle'"
-              src="@/assets/img/sertifikat excle_001.jpg"
-              class="img-fluid"
-            />
-            <img
-              v-else-if="portofolio.class === 'ks-brevet'"
-              src="@/assets/img/Scan Brevet_001.jpg"
-              class="img-fluid"
-            />
-            <img
-              v-else-if="portofolio.class === 'ak-pelaksana'"
-              src="@/assets/img/1.Pelaksana_001.jpg"
-              class="img-fluid"
-            />
-            <img
-              v-else-if="portofolio.class === 'ak-menengah'"
-              src="@/assets/img/2.Menengah_001.jpg"
-              class="img-fluid ak-menengah"
-            />
-            <img
-              v-else-if="portofolio.class === 'ak-lanjutan'"
-              class="img-fluid"
-              src="@/assets/img/3.Lanjutan_001.jpg"
-            />
-            
-            <div class="portofolio-wrap">
-              <div class="portofolio-info">
-                <div class="portofolio-name">
-                  <h4> {{ portofolio.name }} </h4>
-                  <p class="my-8 font-sm">{{ portofolio.publisher }}</p>
-                  <a class="link-view" @click.stop.prevent="showDetail(portofolio)">
-                    view
-                  </a>
-                </div>
+        <div
+          v-for="(portofolio, n) in portofolioFiltered"
+          :key="'port' + n"
+          class="col portofolio my-8">
+          <img
+            :src="`/mrsidik/static/img/${portofolio.img}`"
+            class="img-fluid"
+          />
+          
+          <div class="portofolio-wrap">
+            <div class="portofolio-info">
+              <div class="portofolio-name">
+                <h4> {{ portofolio.name }} </h4>
+                <p class="my-8 font-sm">{{ portofolio.publisher }}</p>
+                <a class="link-view" @click.stop.prevent="showDetail(portofolio)">
+                  view
+                </a>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </transition>
   </div>
